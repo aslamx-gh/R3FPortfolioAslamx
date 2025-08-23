f22591e6eb2f4490dcb7ebf9b8b58b5ec8d49b88
aslamx-gh <aslamx-github@protonmail.com>
2025-08-23 20:08:10 +0200

    Initial commit: add Next.js project setup with configs and README

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.exr$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: 'image/x-exr',
+                },
+                experimental: {
+                    reactRefresh: false,
+                },
+            },
+        });
+        return config;
+    },
+};
 
 export default nextConfig;
